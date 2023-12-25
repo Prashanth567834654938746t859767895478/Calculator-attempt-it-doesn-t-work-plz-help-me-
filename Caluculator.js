db53838beb0bfc19e9f0b10e68db439747ded665
Prashanth567834654938746t859767895478 <144716837+Prashanth567834654938746t859767895478@users.noreply.github.com>
2023-12-25 20:12:45 +0530

    Add files via upload

---
+++ Caluculator.js
@@ -0,0 +1,18 @@
+var q1 = prompt("If you want to multiply type * divide / add + subract -");
+var num1 = prompt("Pick a Number");
+var num2 = prompt("Pick a another Number");
+if (q1 = "*"){
+    alert(num1 * num2);
+};
+
+if (q1 = "/"){
+    alert(num1 / num2);
+};
+
+if (q1 = "+"){
+    alert(num1 + num2);
+};
+
+if (q1 = "-"){
+    alert(num1 - num2);
+};
